3d949245244d40751043d20e6d081f6d011187ed
Kaicapewell1 <kaijcapewell@gmail.com>
2026-01-21 20:48:46 +0000

    creates random walker

--- map_gen.py
+++ map_gen.py
@@ -1,26 +1,40 @@
 import tkinter as tk
+import random as r
 
 
 CELL = 20
-COLS = 20
-ROWS = 20
+COLS = 40
+ROWS = 40
+whitespace = 2
 
 
-# Creating Window
+def placeWalker():
+    random_row = r.randint(0, ROWS - whitespace)
+    random_col = r.randint(0, COLS -whitespace)
+    
+    return(random_col, random_row)
+
+def create_grid(random_cell): 
+    lable = tk.Label(win, text=f"{random_cell}", font=("Arial", 16))
+    lable.pack()
+    canvas = tk.Canvas(win, width=COLS*CELL, height=ROWS*CELL)
+    canvas.pack(expand=True)
+
+    for row in range(ROWS + 1):
+        canvas.create_line(0, row*CELL, COLS*CELL, row*CELL)
+
+    for col in range(COLS + 1):
+        canvas.create_line(col*CELL, 0 ,col*CELL, ROWS*CELL)
+
+
 win = tk.Tk()
 
+random_cell = placeWalker()
 
-
-canvas = tk.Canvas(win, width=COLS*CELL, height=ROWS*CELL)
-canvas.pack(expand=True)
-
-
-for r in range(ROWS + 1):
-    canvas.create_line(0, r*CELL, COLS*CELL, r*CELL)
-
-for c in range(COLS + 1):
-    canvas.create_line(c*CELL, 0 ,c*CELL, ROWS*CELL)
-
+create_grid(random_cell)
 win.mainloop()
+
+
+
 
 
